2a4cbebd04d970ee99dcc146673f0a00299feff9
IT21356186 <shashitha20kavishwara@gmail.com>
2024-05-03 13:44:22 +0530

    after update movie controller

--- src/main/java/com/security/sample/controller/MovieController.java
+++ src/main/java/com/security/sample/controller/MovieController.java
@@ -1,21 +1,24 @@
 package com.security.sample.controller;
 
 
-import com.security.sample.dto.CinemaBookingPaymentDto;
-import com.security.sample.dto.MovieBookingDto;
-import com.security.sample.entity.CinemaBooking;
-import com.security.sample.entity.Movie;
-import com.security.sample.entity.Feedback;
-import com.security.sample.entity.Payment;
-import com.security.sample.service.BookingService;
-import com.security.sample.service.MovieService;
-import com.security.sample.service.FeedbackService;
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
+import com.security.sample.dto.MovieBookingDto;
+import com.security.sample.entity.Movie;
+import com.security.sample.service.BookingService;
+import com.security.sample.service.MovieService;
 
 @RestController
 @CrossOrigin(origins = "http://localhost:4200")
@@ -52,6 +55,13 @@ public class MovieController {
             return ResponseEntity.ok(movies);
         }
     }
+    // Delete movie by ID
+@DeleteMapping("/delete-movie/{id}")
+public ResponseEntity<String> deleteMovie(@PathVariable long id) {
+    movieService.deleteMovie(id);
+    return ResponseEntity.ok("Movie with ID " + id + " deleted successfully");
+}
+
 
 
 //--------------------------------------------------------------Booking
